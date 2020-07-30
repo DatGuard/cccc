7d44817b58f083562fb2aec8c64edb03bc6a8dc2
DatGuard <67444984+DatGuard@users.noreply.github.com>
2020-07-31 00:03:46 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,272 @@
+questions = [
+    {
+        "question": "Private Property should exist."
+        "effect": {
+          "econ": 5,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+    {
+        "question": "An individual by himself is useless."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 5,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+    {
+        "question": "My culture is superior"
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 5,
+		  "mili": 0
+		  
+        }
+    },
+    {
+        "question": "All traditions deserve the same amount of respect."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 5,
+		  "mili": 0
+		  
+        }
+    },
+    {
+        "question": "It would be fine if I were to murder an entire family of innocent people."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 10,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "I wouldn't mind if people die at large amounts as long as they're not related to me."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": -5.
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "Egoism is not necessarily bad."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": -5,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "The freer the market, the freer the people.
+        "effect": {
+          "econ": 5,
+		  "dipl": 0,
+		  "civi": -5,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "If you want to live in my country, you should adopt it's culture."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 5,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "Humans are united by nature and should not act alone."
+        "effect": {
+          "econ": -5,
+		  "dipl": 0,
+		  "civi": 5,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "I don't want to get related with outsiders."
+        "effect": {
+          "econ": 0,
+		  "dipl": 5,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 5,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "Money is baseless idea."
+        "effect": {
+          "econ": -5,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "Important things like education and healthcare should be public."
+        "effect": {
+          "econ": -5,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": 
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": 0,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+	    {
+        "question": "Humanity was a mistake."
+        "effect": {
+          "econ": 0,
+		  "dipl": 0,
+		  "civi": 0,
+		  "morl": 0,
+		  "ordr": 0,
+		  "tech": 0,
+		  "hier": 0,
+		  "rlgs": -5,
+		  "rgnl": 0,
+		  "govt": 0,
+		  "cult": 0,
+		  "mili": 0
+		  
+        }
+    },
+];
